4fe5aaa23822418e18c96930d142c000d9d0e86c
testerjp <testerjp.github@gmail.com>
2014-03-08 00:48:27 +0900

    move type defiitions to header

--- src/KernTable.h
+++ src/KernTable.h
@@ -13,11 +13,49 @@
 
 U_NAMESPACE_BEGIN
 
-struct PairInfo;
-
 /**
  * Windows type 0 kerning table support only for now.
  */
+
+struct PairInfo {
+    le_uint16 left;  // left glyph of kern pair
+    le_uint16 right; // right glyph of kern pair
+    le_int16  value; // fword, kern value in funits
+};
+#define KERN_PAIRINFO_SIZE 6
+LE_CORRECT_SIZE(PairInfo, KERN_PAIRINFO_SIZE)
+
+#define SWAP_KEY(p) (((le_uint32) SWAPW((p)->left) << 16) | SWAPW((p)->right))
+
+struct Subtable_0 {
+    le_uint16 nPairs;
+    le_uint16 searchRange;
+    le_uint16 entrySelector;
+    le_uint16 rangeShift;
+};
+#define KERN_SUBTABLE_0_HEADER_SIZE 8
+LE_CORRECT_SIZE(Subtable_0, KERN_SUBTABLE_0_HEADER_SIZE)
+
+struct SubtableHeader {
+    le_uint16 version;
+    le_uint16 length;
+    le_uint16 coverage;
+};
+#define KERN_SUBTABLE_HEADER_SIZE 6
+LE_CORRECT_SIZE(SubtableHeader, KERN_SUBTABLE_HEADER_SIZE)
+
+struct KernTableHeader {
+    le_uint16 version;
+    le_uint16 nTables;
+};
+#define KERN_TABLE_HEADER_SIZE 4
+LE_CORRECT_SIZE(KernTableHeader, KERN_TABLE_HEADER_SIZE)
+
+#define COVERAGE_HORIZONTAL 0x1
+#define COVERAGE_MINIMUM    0x2
+#define COVERAGE_CROSS      0x4
+#define COVERAGE_OVERRIDE   0x8
+
 class U_LAYOUT_API KernTable
 {
 private:
